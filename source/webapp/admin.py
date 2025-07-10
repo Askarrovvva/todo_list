4764a2e6acbd372e9d1e063b1fdfcddbe40285d1
Askarova Aigerim <askarovaaigerim67@gmail.com>
2025-07-11 02:57:27 +0800

    Поменяла связь на многие ко многим, сделала миграцию

--- source/webapp/admin.py
+++ source/webapp/admin.py
@@ -1,33 +1,24 @@
 from django.contrib import admin
 
-from webapp.models import Issue
+from webapp.models import Issue, Status, Type
 
 
-class TypeAdmin(admin.ModelAdmin):
-    list_display = ['title']
-    list_filter = ['title']
-    list_display_links = ["title"]
-    search_fields = ['title']
-    fields = ['title']
-    readonly_fields = ['title']
-
-
-class StatusAdmin(admin.ModelAdmin):
-    list_display = ['title']
-    list_filter = ['title']
-    list_display_links = ["title"]
-    search_fields = ['title']
-    fields = ['title']
-    readonly_fields = ['title']
+class TypeInline(admin.TabularInline):
+    model = Issue.types.through
 
 
 class IssueAdmin(admin.ModelAdmin):
-    list_display = ['id', 'description', 'statuses', 'summary']
-    list_filter = ['statuses', 'id', 'types']
-    list_display_links = ["description"]
+    list_display = ['id', 'description', 'statuses', 'summary', 'created_at', 'updated_at']
+    list_filter = ['statuses', 'id', 'statuses']
+    list_display_links = ["description", 'statuses']
     search_fields = ['description', 'statuses']
     fields = ['description', 'statuses']
-    readonly_fields = ['types']
+    readonly_fields = ['created_at', 'updated_at']
+    inlines = [
+        TypeInline,
+    ]
 
 
-admin.site.register(Issue, IssueAdmin, TypeAdmin, StatusAdmin)
+admin.site.register(Issue, IssueAdmin)
+admin.site.register(Status)
+admin.site.register(Type)
