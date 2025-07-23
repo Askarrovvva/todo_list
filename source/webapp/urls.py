92fb370e4258f6cd1a36a796a1d835e465934e8a
Askarova Aigerim <askarovaaigerim67@gmail.com>
2025-07-23 14:12:51 +0800

    Подправила urls

--- source/webapp/urls.py
+++ source/webapp/urls.py
@@ -1,11 +1,17 @@
 from django.urls import path
-from webapp.views.issues import IssueListView, CreateIssueView, IssueDetailView, DeleteIssueView, UpdateIssueView
 
+from webapp.views.issues import IssueDetailView, IssueDeleteView, IssueUpdateView, IssueCreateView
+from webapp.views.projects import ProjectDeleteView, ProjectUpdateView, ProjectCreateView, ProjectDetailView, \
+    ProjectListView
 
 urlpatterns = [
-    path('', IssueListView.as_view(), name='main'),
-    path('issue/create/', CreateIssueView.as_view(), name='create'),
+    path('', ProjectListView.as_view(), name='main'),
+    path('project/<int:pk>/delete/', ProjectDeleteView.as_view(), name='delete_project'),
+    path('project/<int:pk>/update/', ProjectUpdateView.as_view(), name='update_project'),
+    path('project/create/', ProjectCreateView.as_view(), name='create_project'),
+    path('project/<int:pk>/', ProjectDetailView.as_view(), name='detail_project'),
     path('issue/<int:pk>/', IssueDetailView.as_view(), name="detail"),
-    path('issue/<int:pk>/update/', UpdateIssueView.as_view(), name='update'),
-    path('issue/<int:pk>/delete/', DeleteIssueView.as_view(), name='delete')
+    path('issue/<int:pk>/update/', IssueUpdateView.as_view(), name='update'),
+    path('issue/<int:pk>/create/', IssueCreateView.as_view(), name='create'),
+    path('delete/<int:pk>/', IssueDeleteView.as_view(), name="delete"),
 ]
